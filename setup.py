c3c04dc33635f0d39a94fdbd2a5ba4a9a92def19
Ondřej Profant <ondrej.profant@gmail.com>
2015-08-02 16:06:25 +0200

    ...

--- setup.py
+++ setup.py
@@ -7,7 +7,8 @@ from setuptools import (setup, find_packages)
 
 # Get the long description from the relevant file
 here = path.abspath(path.dirname(__file__))
-with open(path.join(here, 'DESCRIPTION.rst'), encoding='utf-8') as f:
+filepath = path.jon(here, 'DESCRIPTION.rst')
+with open(filepath, encoding='utf8') as f:
     long_description = f.read()
 
 
