c3d8348d6598bf5575b18f414a03afdfb7f9b637
Ondřej Profant <ondrej.profant@gmail.com>
2015-09-25 17:35:06 +0200

    Final version of resource handling

--- pyexample/__init__.py
+++ pyexample/__init__.py
@@ -11,13 +11,13 @@ Python example project. Example of:
 * unit tests via standard `unittest` module
 """
 
+__version__ = "0.4.3"
+__status__ = "Beta"
 __author__ = "Ondřej Profant"
-__copyright__ = "Copyright 2015, Ondřej Profant"
-__credits__ = ["Ondřej Profant"]
 __license__ = "Affero GNU-GPL v3"
-__version__ = "0.4.2"
+__credits__ = ["Ondřej Profant"]
 __maintainer__ = "Ondřej Profant"
 __email__ = "ondrej.profant@gmal.com"
-__status__ = "Beta"
+__copyright__ = "Copyright 2015, Ondřej Profant"
 
 __all__ = ["hello"]
